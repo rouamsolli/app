a784448bf1fe1a4aa04c8d727a5360c71aa7b45e
rouamsolli <112184084+rouamsolli@users.noreply.github.com>
2022-08-26 16:38:39 +0100

    Create Images.js

---
+++ src/Theme/themes/default_dark/Images.js
@@ -0,0 +1,11 @@
+/**
+ * Images should be stored in the `App/Images` directory and referenced using variables defined here.
+ */
+
+export default function () {
+  return {
+    logo: {
+      uri: 'https://www.shutterstock.com/fr/image-vector/chat-message-icon-talk-bubble-speech-1932688535',
+    },
+  }
+}
